5e996f8aaded4c1a3dce5d578abd3f66ed342f6f
Shivam Sharma <shivam15951@gmail.com>
2020-05-25 20:19:45 +0530

    Updates

---
+++ precache-manifest.dd98d60535eb57e4138b100d5dce6f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa13cba206ba3b52716f55710c4f6563",
+    "url": "/GitHub-User-Info/index.html"
+  },
+  {
+    "revision": "042ccd0166fc653c9feb",
+    "url": "/GitHub-User-Info/static/css/main.d7f97fb6.chunk.css"
+  },
+  {
+    "revision": "a48ea74a8fea29756b50",
+    "url": "/GitHub-User-Info/static/js/2.7992bed8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GitHub-User-Info/static/js/2.7992bed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "042ccd0166fc653c9feb",
+    "url": "/GitHub-User-Info/static/js/main.68afee86.chunk.js"
+  },
+  {
+    "revision": "a5e87bef8d7bf5ecff50",
+    "url": "/GitHub-User-Info/static/js/runtime-main.f272941d.js"
+  }
+]);
